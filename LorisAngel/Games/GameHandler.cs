98837185a81f267eb0f28ddd3dd4e2c24d54db34
byBlurr <dbldumont11@gmail.com>
2021-01-20 17:26:14 +0000

    Add code for checking if game exists

--- LorisAngel/Games/GameHandler.cs
+++ LorisAngel/Games/GameHandler.cs
@@ -8,6 +8,19 @@ namespace LorisAngel.Games
     {
         private static List<Game> OngoingGames = new List<Game>();
 
+        public static bool DoesGameExist(ulong gameId)
+        {
+            foreach (Game game in OngoingGames)
+            {
+                if (game.GameId == gameId)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
         public static void AddNewGame(Game game)
         {
             if (!OngoingGames.Contains(game)) OngoingGames.Add(game);
@@ -18,11 +31,11 @@ namespace LorisAngel.Games
             if (OngoingGames.Contains(game)) OngoingGames.Remove(game);
         }
 
-        public static ulong CheckForWinner(ulong GameId)
+        public static ulong CheckForWinner(ulong gameId)
         {
             foreach (Game game in OngoingGames)
             {
-                if (game.GameId == GameId)
+                if (game.GameId == gameId)
                 {
                     return game.CheckForWinner();
                 }
@@ -31,11 +44,11 @@ namespace LorisAngel.Games
             return 0L;
         }
 
-        public static void TakeTurn(ulong GameId, int param1, int param2)
+        public static void TakeTurn(ulong gameId, int param1, int param2)
         {
             foreach (Game game in OngoingGames)
             {
-                if (game.GameId == GameId)
+                if (game.GameId == gameId)
                 {
                     game.TakeTurn(param1, param2);
                 }

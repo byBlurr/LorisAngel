31a7d63b9d828f83864440ea4313302e630f8f82
byBlurr <dbldumont11@gmail.com>
2021-01-20 16:01:06 +0000

    Added temp bans

--- LorisAngel/CommandModules/ModeratorModule.cs
+++ LorisAngel/CommandModules/ModeratorModule.cs
@@ -2,6 +2,8 @@
 using Discord.Commands;
 using Discord.Net.Bot;
 using Discord.Net.Bot.Database.Configs;
+using LorisAngel.Database;
+using System;
 using System.Threading.Tasks;
 
 namespace LorisAngel.CommandModules
@@ -68,6 +70,41 @@ namespace LorisAngel.CommandModules
             EmbedBuilder embed = new EmbedBuilder()
             {
                 Title = user.Username + " was banned",
+                Description = reason,
+                Color = Color.DarkPurple,
+                Footer = new EmbedFooterBuilder() { Text = $"{Util.GetRandomEmoji()}  Edit moderation settings on the webpanel." }
+            };
+
+            await CreateLogAsync(gconf, embed);
+        }
+
+        [Command("tempban")]
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        [RequireBotPermission(ChannelPermission.SendMessages)]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [RequireBotPermission(GuildPermission.BanMembers)]
+        private async Task TempBanMemberAsync(IUser user = null, int time = 60, string reason = null)
+        {
+            await Context.Message.DeleteAsync();
+
+            BotConfig conf = BotConfig.Load();
+            var gconf = conf.GetConfig(Context.Guild.Id);
+
+            if (user == null)
+            {
+                await Util.SendErrorAsync((Context.Channel as ITextChannel), "Incorrect Command Usage", $"Correct Usage: `{gconf.Prefix}ban <user> <time> [reason]`", false);
+                return;
+            }
+
+            if (reason == null) reason = "Banned by " + Context.User.Username + "#" + Context.User.Discriminator + " for " + time + " minutes";
+            else reason += " - Banned by " + Context.User.Username + "#" + Context.User.Discriminator + " for " + time + " minutes";
+
+            await BanMemberAsync(user, reason);
+            await ModerationDatabase.AddTempBanAsync(Context.Guild.Id, user.Id, DateTime.Now.AddMinutes(time));
+
+            EmbedBuilder embed = new EmbedBuilder()
+            {
+                Title = user.Username + " was temp banned",
                 Description = reason,
                 Color = Color.DarkPurple,
                 Footer = new EmbedFooterBuilder() { Text = $"{Util.GetRandomEmoji()}  Edit moderation settings on the webpanel." }
---
+++ LorisAngel/Database/ModerationDatabase.cs
@@ -0,0 +1,121 @@
+﻿using Discord;
+using Discord.Net.Bot;
+using Discord.Net.Bot.Database.Configs;
+using Discord.Net.Bot.Database.Sql;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LorisAngel.Database
+{
+    class ModerationDatabase
+    {
+        /***
+         *  Remove any bans that have timed out
+         */
+        public static async Task ProcessBansAsync()
+        {
+            var processBans = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var dbCon = DBConnection.Instance();
+                    dbCon.DatabaseName = LCommandHandler.DATABASE_NAME;
+                    BotConfig conf = BotConfig.Load();
+
+                    if (dbCon.IsConnect())
+                    {
+                        var cmd = new MySqlCommand($"SELECT * FROM tempbans", dbCon.Connection);
+                        var reader = cmd.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                DateTime bannedTill = reader.GetDateTime(2);
+                                if (bannedTill < DateTime.Now)
+                                {
+                                    ulong guildid = reader.GetUInt64(0);
+                                    ulong userid = reader.GetUInt64(1);
+
+                                    var guild = CommandHandler.GetBot().GetGuild(guildid);
+
+                                    if (guild != null)
+                                    {
+                                        var gconf = conf.GetConfig(guild.Id);
+                                        if (gconf.LogActions)
+                                        {
+                                            var logs = guild.GetTextChannel(gconf.LogChannel);
+                                            if (logs != null)
+                                            {
+                                                EmbedBuilder embed = new EmbedBuilder()
+                                                {
+                                                    Title = "User Unbanned",
+                                                    Description = $"A users temp ban was removed. Check audit log for more info.",
+                                                    Color = Color.DarkPurple,
+                                                    Footer = new EmbedFooterBuilder() { Text = $"{Util.GetRandomEmoji()}  Edit moderation settings on the webpanel." }
+                                                };
+                                                await logs.SendMessageAsync(null, false, embed.Build());
+                                            }
+                                        }
+
+                                        await guild.RemoveBanAsync(userid);
+                                    }
+
+                                    var removecmd = new MySqlCommand($"REMOVE FROM tempbans WHERE userid = '{userid}'", dbCon.Connection);
+                                    try
+                                    {
+                                        await removecmd.ExecuteNonQueryAsync();
+                                        removecmd.Dispose();
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine($"Failed to remove temp ban: {e.Message}");
+                                        removecmd.Dispose();
+                                    }
+                                }
+                            }
+                        }
+
+                        reader.Close();
+                        cmd.Dispose();
+                        dbCon.Close();
+                    }
+
+                    await Task.Delay(60000);
+                }
+            });
+        }
+
+        /***
+         *  Add the temp ban to the database
+         */
+        public static async Task AddTempBanAsync(ulong guildId, ulong userId, DateTime bannedTill)
+        {
+            var dbCon = DBConnection.Instance();
+            dbCon.DatabaseName = LCommandHandler.DATABASE_NAME;
+
+            if (dbCon.IsConnect())
+            {
+                var cmd = new MySqlCommand($"INSERT INTO users (guildid, userid, bannedtill) VALUES (@guildid, @userid, @bannedtill)", dbCon.Connection);
+                cmd.Parameters.Add("@guildid", MySqlDbType.UInt64).Value = guildId;
+                cmd.Parameters.Add("@userid", MySqlDbType.UInt64).Value = userId;
+                cmd.Parameters.Add("@bannedtill", MySqlDbType.DateTime).Value = bannedTill;
+
+                try
+                {
+                    await cmd.ExecuteNonQueryAsync();
+                    cmd.Dispose();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Failed to add temp ban: {e.Message}");
+                    cmd.Dispose();
+                }
+
+                dbCon.Close();
+            }
+        }
+    }
+}

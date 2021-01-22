f0c9bf282c4cb50b1cd929d8c04817b479a94a48
byBlurr <dbldumont11@gmail.com>
2021-01-22 14:07:04 +0000

    Start currency commands

---
+++ LorisAngel/CommandModules/CurrencyModule.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace LorisAngel.CommandModules
+{
+    public class CurrencyModule : ModuleBase
+    {
+        [Command("bank")]
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        [RequireBotPermission(ChannelPermission.SendMessages)]
+        private async Task BankAsync()
+        {
+            await Context.Message.DeleteAsync();
+
+
+        }
+
+        [Command("bank transfer")]
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        [RequireBotPermission(ChannelPermission.SendMessages)]
+        private async Task BankTransferAsync(IUser user = null, int amount = 0)
+        {
+            await Context.Message.DeleteAsync();
+
+
+        }
+    }
+}

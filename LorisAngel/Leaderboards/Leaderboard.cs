82c02e3feb55094040bcb1ddfad56dea26d74ca1
byBlurr <dbldumont11@gmail.com>
2021-01-23 16:35:41 +0000

    Start work on leaderboards

---
+++ LorisAngel/Leaderboards/Leaderboard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LorisAngel.Leaderboards
+{
+    public abstract class Leaderboard
+    {
+        private static List<LeaderboardRow> Rows;
+
+    }
+
+    public class LeaderboardRow
+    {
+        public ulong Id { get; private set; }
+        public string Name { get; private set; }
+        public int Score { get; private set; }
+
+        public LeaderboardRow(ulong id, string name, int score)
+        {
+            Id = id;
+            Name = name;
+            Score = score;
+        }
+    }
+}

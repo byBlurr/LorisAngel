28824fe9cdf10e85fd2301d3eb74eb95e60847e1
byBlurr <dbldumont11@gmail.com>
2021-01-24 12:43:36 +0000

    Start work on leaderboard database

---
+++ LorisAngel/Database/LeaderboardDatabase.cs
@@ -0,0 +1,22 @@
+﻿using LorisAngel.Leaderboards;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LorisAngel.Database
+{
+    public class LeaderboardDatabase
+    {
+        // Add score
+        public static void AddScore(ulong userId, string userName, string lbName)
+        {
+
+        }
+
+        // Get leaderboard
+        public static Leaderboard GetLeaderboard(string lbName, int count = 10)
+        {
+            return null;
+        }
+    }
+}

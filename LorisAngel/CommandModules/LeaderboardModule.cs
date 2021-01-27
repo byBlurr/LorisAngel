972282bd7e7bf473a729b978c611b49a8744966d
byBlurr <dbldumont11@gmail.com>
2021-01-27 13:54:07 +0000

    Add leaderboard command

--- LorisAngel/CommandModules/LeaderboardModule.cs
+++ LorisAngel/CommandModules/LeaderboardModule.cs
@@ -1,8 +1,17 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
 
 namespace LorisAngel.CommandModules
 {
     public class LeaderboardModule : ModuleBase
     {
+        [Command("leaderboard")]
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        [RequireBotPermission(ChannelPermission.SendMessages)]
+        private async Task LeaderboardAsync([Remainder] string lb = "")
+        {
+            await Context.Message.DeleteAsync();
+        }
     }
 }

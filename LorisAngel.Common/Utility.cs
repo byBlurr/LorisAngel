4c706980763d2890c56daed43bae009fb1dfab96
byBlurr <dbldumont11@gmail.com>
2021-02-10 16:20:00 +0000

    Add Utility.GetBotInviteLink()

--- LorisAngel.Common/Utility.cs
+++ LorisAngel.Common/Utility.cs
@@ -18,5 +18,16 @@
 
             return char.ToUpper(s[0]) + s.Substring(1).ToLower();
         }
+
+        /// <summary>
+        /// Get the invite link to add the bot to the server
+        /// </summary>
+        /// <param name="clientid">The client ID of the bot. The default is LorisAngel</param>
+        /// <param name="permissions">The permissions to return. Default is LorisAngel perms</param>
+        /// <returns>Returns a string of a url for the invite link</returns>
+        public static string GetBotInviteLink(ulong clientid = 729696788097007717, int permissions = 44032)
+        {
+            return $"https://discordapp.com/oauth2/authorize?client_id={clientid}&scope=bot&permissions={permissions}";
+        }
     }
 }

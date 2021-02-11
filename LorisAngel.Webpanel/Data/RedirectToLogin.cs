aa73048367b3282d904ca750d7bbb5a1888caea8
byBlurr <dbldumont11@gmail.com>
2021-02-11 09:21:02 +0000

    Require authentication

---
+++ LorisAngel.Webpanel/Data/RedirectToLogin.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace LorisAngel.Webpanel.Data
+{
+    public class RedirectToLogin : ComponentBase
+    {
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+            NavigationManager.NavigateTo("/login", true);
+        }
+    }
+}

2186ae9422383901e437056fc016d6e389c19646
byBlurr <dbldumont11@gmail.com>
2021-02-12 17:11:25 +0000

    Start work on ProfileRenderer

---
+++ LorisAngel.Common/Rendering/ProfileRenderer.cs
@@ -0,0 +1,30 @@
+﻿using LorisAngel.Common.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LorisAngel.Common.Rendering
+{
+    public class ProfileRenderer
+    {
+
+        private LoriUser User { get; set; }
+        private ProfileData Data { get; set; }
+
+        public ProfileRenderer(LoriUser user)
+        {
+            User = user;
+            GetProfileData();
+        }
+
+        public void Render()
+        {
+
+        }
+
+        public void GetProfileData()
+        {
+
+        }
+    }
+}

1f58a93f6a4f34eac072cc83af737acc34d2cfc3
byBlurr <dbldumont11@gmail.com>
2021-02-12 17:37:25 +0000

    Use Blurr.Sql to get ProfileData

--- LorisAngel.Common/Rendering/ProfileRenderer.cs
+++ LorisAngel.Common/Rendering/ProfileRenderer.cs
@@ -1,4 +1,6 @@
-﻿using LorisAngel.Common.Objects;
+﻿using Blurr;
+using Blurr.Sql;
+using LorisAngel.Common.Objects;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -15,6 +17,7 @@ namespace LorisAngel.Common.Rendering
         {
             User = user;
             GetProfileData();
+            Console.WriteLine(Data.ToString());
         }
 
         public void Render()
@@ -24,7 +27,18 @@ namespace LorisAngel.Common.Rendering
 
         public void GetProfileData()
         {
+            var connection = SqlConnection.Instance();
+            connection.DatabaseName = "lorisangel";
+            string[] columns = { "background", "avatar" };
 
+            try
+            {
+                Data = SqlHelper.SelectDataFromTable<ProfileData>(connection, "profiles", columns, $"id = '{User.Id}'")[0];
+            }
+            catch (Exception ex)
+            {
+                Util.Log(LogType.Error, "MySQL", ex.Message);
+            }
         }
     }
 }

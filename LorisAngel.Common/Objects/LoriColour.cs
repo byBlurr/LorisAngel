fbec8ec8fba61b771adc4703f53c177bedd35f13
byBlurr <dbldumont11@gmail.com>
2021-02-12 18:55:13 +0000

    Add ToDrawColor() to LoriColour

--- LorisAngel.Common/Objects/LoriColour.cs
+++ LorisAngel.Common/Objects/LoriColour.cs
@@ -1,11 +1,14 @@
-﻿namespace LorisAngel.Common.Objects
+﻿using System;
+using System.Drawing;
+
+namespace LorisAngel.Common.Objects
 {
     public class LoriColour
     {
 
         public static LoriColour LoriPurple = new LoriColour(121, 55, 134, "#793786");
         public static LoriColour LoriRed = new LoriColour(231, 74, 72, "#e74a48");
-        public static LoriColour LoriGreen = new LoriColour(231, 74, 72, "#00b485");
+        public static LoriColour LoriGreen = new LoriColour(0, 180, 133, "#00b485");
         public static LoriColour Blurple = new LoriColour(114, 137, 218, "#7289DA");
         public static LoriColour FullWhite = new LoriColour(255, 255, 255, "#FFFFFF");
         public static LoriColour Greyple = new LoriColour(153, 170, 181, "#99AAB5");
@@ -25,6 +28,8 @@
             Blue = b;
             Hex = hex;
         }
+
+        public Color ToDrawColor() => Color.FromArgb(Red, Green, Blue);
 
         // TODO: We probably want a way to convert this into a Discord Color?
         // public Discord.Color ConvertToDiscord() => new Discord.Color(r, g, b);

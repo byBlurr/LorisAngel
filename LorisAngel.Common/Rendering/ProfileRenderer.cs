e0fe7b7782b59f2a2ffc42fab4847d69c3a2ab8d
byBlurr <dbldumont11@gmail.com>
2021-02-13 14:05:07 +0000

    Adjusted font sizes on profile

--- LorisAngel.Common/Rendering/ProfileRenderer.cs
+++ LorisAngel.Common/Rendering/ProfileRenderer.cs
@@ -44,14 +44,14 @@ namespace LorisAngel.Common.Rendering
             Bitmap backTexture = new Bitmap(backgroundFile);
             graphicImage.DrawImage(backTexture, 0, 0, width, height);
 
-            graphicImage.DrawString(User.Name, new Font(fontName, 32, FontStyle.Bold), darkBrush, ProfileLocations.Name.X + (ProfileLocations.Name.Width / 2), ProfileLocations.Name.Y + (ProfileLocations.Name.Height / 2), format);
-            graphicImage.DrawString(User.Status, new Font(fontName, 18, FontStyle.Bold), darkBrush, ProfileLocations.Status.X + (ProfileLocations.Status.Width / 2), ProfileLocations.Status.Y + (ProfileLocations.Status.Height / 2), format);
-            graphicImage.DrawString(User.Motto, new Font(fontName, 18, FontStyle.Bold), darkBrush, ProfileLocations.Motto.X + (ProfileLocations.Motto.Width / 2), ProfileLocations.Motto.Y + (ProfileLocations.Motto.Height / 2), format);
-            graphicImage.DrawString(User.CreatedOn.ToString(), new Font(fontName, 18, FontStyle.Bold), darkBrush, ProfileLocations.CreatedOn.X + (ProfileLocations.CreatedOn.Width / 2), ProfileLocations.CreatedOn.Y + (ProfileLocations.CreatedOn.Height / 2), format);
-            graphicImage.DrawString(User.LastSeen.ToString(), new Font(fontName, 18, FontStyle.Bold), darkBrush, ProfileLocations.LastSeen.X + (ProfileLocations.LastSeen.Width / 2), ProfileLocations.LastSeen.Y + (ProfileLocations.LastSeen.Height / 2), format);
-            graphicImage.DrawString(User.JoinedOn.ToString(), new Font(fontName, 18, FontStyle.Bold), darkBrush, ProfileLocations.JoinedOn.X + (ProfileLocations.JoinedOn.Width / 2), ProfileLocations.JoinedOn.Y + (ProfileLocations.JoinedOn.Height / 2), format);
-            graphicImage.DrawString(User.LastUpdated.ToString(), new Font(fontName, 18, FontStyle.Bold), darkBrush, ProfileLocations.UpdatedOn.X + (ProfileLocations.UpdatedOn.Width / 2), ProfileLocations.UpdatedOn.Y + (ProfileLocations.UpdatedOn.Height / 2), format);
-            graphicImage.DrawString(Id.ToString(), new Font(fontName, 18, FontStyle.Bold), darkBrush, ProfileLocations.Id.X + (ProfileLocations.Id.Width / 2), ProfileLocations.Id.Y + (ProfileLocations.Id.Height / 2), format);
+            graphicImage.DrawString(User.Name, new Font(fontName, 30, FontStyle.Bold), darkBrush, ProfileLocations.Name.X + (ProfileLocations.Name.Width / 2), ProfileLocations.Name.Y + (ProfileLocations.Name.Height / 2), format);
+            graphicImage.DrawString(User.Status, new Font(fontName, 14, FontStyle.Bold), darkBrush, ProfileLocations.Status.X + (ProfileLocations.Status.Width / 2), ProfileLocations.Status.Y + (ProfileLocations.Status.Height / 2), format);
+            graphicImage.DrawString(User.Motto, new Font(fontName, 14, FontStyle.Bold), darkBrush, ProfileLocations.Motto.X + (ProfileLocations.Motto.Width / 2), ProfileLocations.Motto.Y + (ProfileLocations.Motto.Height / 2), format);
+            graphicImage.DrawString(User.CreatedOn.ToString(), new Font(fontName, 14, FontStyle.Bold), darkBrush, ProfileLocations.CreatedOn.X + (ProfileLocations.CreatedOn.Width / 2), ProfileLocations.CreatedOn.Y + (ProfileLocations.CreatedOn.Height / 2), format);
+            graphicImage.DrawString(User.LastSeen.ToString(), new Font(fontName, 14, FontStyle.Bold), darkBrush, ProfileLocations.LastSeen.X + (ProfileLocations.LastSeen.Width / 2), ProfileLocations.LastSeen.Y + (ProfileLocations.LastSeen.Height / 2), format);
+            graphicImage.DrawString(User.JoinedOn.ToString(), new Font(fontName, 14, FontStyle.Bold), darkBrush, ProfileLocations.JoinedOn.X + (ProfileLocations.JoinedOn.Width / 2), ProfileLocations.JoinedOn.Y + (ProfileLocations.JoinedOn.Height / 2), format);
+            graphicImage.DrawString(User.LastUpdated.ToString(), new Font(fontName, 14, FontStyle.Bold), darkBrush, ProfileLocations.UpdatedOn.X + (ProfileLocations.UpdatedOn.Width / 2), ProfileLocations.UpdatedOn.Y + (ProfileLocations.UpdatedOn.Height / 2), format);
+            graphicImage.DrawString(Id.ToString(), new Font(fontName, 14, FontStyle.Bold), darkBrush, ProfileLocations.Id.X + (ProfileLocations.Id.Width / 2), ProfileLocations.Id.Y + (ProfileLocations.Id.Height / 2), format);
 
             editedBitmap.Save(GetPath(), System.Drawing.Imaging.ImageFormat.Png);
             graphicImage.Dispose();

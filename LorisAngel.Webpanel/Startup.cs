d61f56b4ec7b636070014f6fd39d0f4c57e130e4
byBlurr <dbldumont11@gmail.com>
2021-02-13 14:37:14 +0000

    [webpanel] Remove some default files

--- LorisAngel.Webpanel/Startup.cs
+++ LorisAngel.Webpanel/Startup.cs
@@ -1,15 +1,9 @@
 using LorisAngel.Webpanel.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LorisAngel.Webpanel
 {
@@ -28,7 +22,6 @@ namespace LorisAngel.Webpanel
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<LeaderboardService>();
         }
 

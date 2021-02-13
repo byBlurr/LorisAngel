360106e2af168183e67f99fb7e9acfed8eabb65f
byBlurr <dbldumont11@gmail.com>
2021-02-13 14:54:01 +0000

    Use database data for leaderboards

--- LorisAngel.Webpanel/Data/LeaderboardService.cs
+++ LorisAngel.Webpanel/Data/LeaderboardService.cs
@@ -1,4 +1,6 @@
-﻿using LorisAngel.Common.Objects;
+﻿using Blurr;
+using Blurr.Sql;
+using LorisAngel.Common.Objects;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -14,18 +16,37 @@ namespace LorisAngel.Webpanel.Data
             string tableName = LoriLeaderboard.GetTableName(lbName);
             List<LeaderboardRow> rows = new List<LeaderboardRow>();
 
-            // Connect to db
 
-            // Get 10 rows in desc order
-            for (int i = 0; i < 10; i++)
+            // Connect to db
+            SqlConnection connection = SqlConnection.Instance();
+            connection.DatabaseName = "lorisangel";
+            // connection.DatabasePassword = ""; Guess we gonna need a local file for this...
+
+            try
             {
-                rows.Add(new LeaderboardRow(0L, "Steve " + i, i * 12));
+                string[] columns = { "id", "name", "score" };
+                var sqlRows = SqlHelper.SelectDataFromTable<LeaderboardDBRow>(connection, tableName, columns);
+
+                foreach (var row in sqlRows)
+                {
+                    rows.Add(new LeaderboardRow((ulong)row.id, row.name, row.score));
+                }
+            }
+            catch (Exception ex)
+            {
+                Util.Log(LogType.Error, "MySql", ex.Message);
             }
 
             // Create leaderboard with rows
             LoriLeaderboard leaderboard = new LoriLeaderboard(lbName, rows);
-
             return leaderboard;
         }
+    }
+
+    public class LeaderboardDBRow
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public int score { get; set; }
     }
 }

8c64d29eecba63ebe225ff12c56e5a2a31c96ac1
byBlurr <dbldumont11@gmail.com>
2021-02-13 14:11:07 +0000

    Add LoriBadge

---
+++ LorisAngel.Common/Objects/LoriBadge.cs
@@ -0,0 +1,10 @@
+﻿namespace LorisAngel.Common.Objects
+{
+    public class LoriBadge
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}
